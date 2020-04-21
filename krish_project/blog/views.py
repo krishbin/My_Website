f8a399c35c73d1c3a418304b8966136b6c988417
Krishbin Paudel <krishbinp@outlook.com>
2020-04-21 17:43:07 +0545

    working

--- krish_project/blog/views.py
+++ krish_project/blog/views.py
@@ -1,27 +1,44 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Post
 # Create your views here.
-posts = [
-    {
-        'author': 'krish',
-        'title': 'Blog Post 1',
-        'content': 'first post content',
-        'date_posted': 'August 27,2020'
-    },
-    {
-        'author': 'nads',
-        'title': 'Blog Post 1',
-        'content': 'first post content',
-        'date_posted': 'August 27,2020'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
